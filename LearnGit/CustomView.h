575e2a483c07a63ae71bc0087e60aef6dac4481c
zbw <zbw@edeng.com>
2015-05-27 10:44:41 +0800

    提交

---
+++ LearnGit/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  LearnGit
+//
+//  Created by edeng-ssd on 15/5/27.
+//  Copyright (c) 2015年 edeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ LearnGit/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  LearnGit
+//
+//  Created by edeng-ssd on 15/5/27.
+//  Copyright (c) 2015年 edeng. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
